bb483ded1f39b522622377eb2b9787b4652bd52d
chlmarchal <chloe.marchal@cytomine.com>
2022-03-10 10:30:04 +0100

    Improved tests (mainly histogram representation testing and code style)

--- tests/test_mrxs.py
+++ tests/test_mrxs.py
@@ -6,110 +6,150 @@ from pims.formats import FORMATS
 import io
 from pims.importer.importer import FileImporter
 
-from tests.utils.formats import info_test, thumb_test, resized_test, mask_test, crop_test, crop_null_annot_test, histogram_perimage_test
-
+from utils.formats import info_test, thumb_test, resized_test, mask_test, crop_test, crop_null_annot_test, histogram_perimage_test
+from pims.formats.utils.factories import FormatFactory
+from pims.api.utils.models import HistogramType
+from pims.processing.histograms.utils import build_histogram_file
 from pims.files.file import (
     EXTRACTED_DIR, HISTOGRAM_STEM, ORIGINAL_STEM, PROCESSED_DIR, Path,
-    SPATIAL_STEM, UPLOAD_DIR_PREFIX, unique_name_generator
+    SPATIAL_STEM, UPLOAD_DIR_PREFIX
 )
 from pims.files.archive import Archive, ArchiveError
 
 import pytest
 
-def get_image(path, image):
-	filepath = os.path.join(path, image)
-	# If image does not exist locally -> download image
-	if not os.path.exists(path):
-		os.mkdir("/data/pims/upload_test_mrxs")
-	
-	if not os.path.exists(filepath):
-		try:
-			url = f"https://data.cytomine.coop/open/openslide/mirax-mrxs/{image}" #OAC
-			urllib.request.urlretrieve(url, filepath)
-		except Exception as e:
-			print("Could not download image")
-			print(e)
-	
-	if not os.path.exists(os.path.join(path, "processed")):
-		try:
-			fi = FileImporter(f"/data/pims/upload_test_mrxs/{image}")
-			fi.upload_dir = "/data/pims/upload_test_mrxs"
-			fi.processed_dir = fi.upload_dir / Path("processed")
-			fi.mkdir(fi.processed_dir)
-		except Exception as e:
-			print(path + "processed could not be created")
-			print(e)
+def get_image(path, filename):
+    filepath = os.path.join(path,filename)
+    # If image does not exist locally -> download image
+    if not os.path.exists(path):
+        os.mkdir("/data/pims/upload_test_mrxs")
 
-	if not os.path.exists(os.path.join(path, "processed/visualisation.MRXS")):
-		try:
-			fi.upload_path = Path(filepath)
-			original_filename = Path(f"{ORIGINAL_STEM}.MRXS")
-			fi.original_path = fi.processed_dir / original_filename
-			archive = Archive.from_path(fi.upload_path)
-			archive.extract(fi.original_path)
-			new_original_path = fi.processed_dir / original_filename
-			fi.move(fi.original_path, new_original_path)
-			fi.original_path = new_original_path
-			fi.upload_path = fi.original_path
-			spatial_filename = Path(f"{SPATIAL_STEM}.MRXS")
-			fi.spatial_path = fi.processed_dir / spatial_filename
-			fi.mksymlink(fi.spatial_path, fi.original_path)
-		except Exception as e:
-			print("Importation of images could not be done")
-			print(e)
-		
+    if not os.path.exists(filepath):
+        try:
+            url = f"https://data.cytomine.coop/open/openslide/mirax-mrxs/{filename}"
+            urllib.request.urlretrieve(url, filepath)
+        except Exception as e:
+            print("Could not download image")
+            print(e)
+
+    if not os.path.exists(os.path.join(path, "processed")):
+        try:
+            fi = FileImporter(f"/data/pims/upload_test_mrxs/{filename}")
+            fi.upload_dir = "/data/pims/upload_test_mrxs"
+            fi.processed_dir = fi.upload_dir / Path("processed")
+            fi.mkdir(fi.processed_dir)
+            print("processed is created")
+        except Exception as e:
+            print(path + "processed could not be created")
+            print(e)
+    if not os.path.exists(os.path.join(path, "processed/visualisation.MRXS")):
+        if os.path.exists(os.path.join(path, "processed")):
+            fi = FileImporter(f"/data/pims/upload_test_mrxs/{filename}")
+            fi.upload_dir = "/data/pims/upload_test_mrxs"
+            fi.processed_dir = fi.upload_dir / Path("processed")
+        try:
+            fi.upload_path = Path(filepath)
+            original_filename = Path(f"{ORIGINAL_STEM}.MRXS")
+            fi.original_path = fi.processed_dir / original_filename # unsupported operand type(s) for /: 'NoneType' and 'Path'
+            archive = Archive.from_path(fi.upload_path)
+            archive.extract(fi.original_path)
+            new_original_path = fi.processed_dir / original_filename
+            fi.move(fi.original_path, new_original_path)
+            fi.original_path = new_original_path
+            fi.upload_path = fi.original_path
+            spatial_filename = Path(f"{SPATIAL_STEM}.MRXS")
+            fi.spatial_path = fi.processed_dir / spatial_filename
+            fi.mksymlink(fi.spatial_path, fi.original_path)
+        except Exception as e:
+            print("Importation of images could not be done")
+            print(e)
+
+    if not os.path.exists(os.path.join(path, "processed/histogram")):
+        if os.path.exists(os.path.join(path, "processed")):
+            fi = FileImporter(f"/data/pims/upload_test_mrxs/{filename}")
+            fi.upload_dir = Path("/data/pims/upload_test_mrxs")
+            fi.processed_dir = fi.upload_dir / Path("processed")
+            original_filename = Path(f"{ORIGINAL_STEM}.MRXS")
+            fi.original_path = fi.processed_dir / original_filename
+        try:
+            from pims.files.image import Image
+            fi.histogram_path = fi.processed_dir/Path(HISTOGRAM_STEM) #/data/pims/upload1641567540187798/processed/histogram
+            format = FormatFactory().match(fi.original_path)
+            fi.original = Image(fi.original_path, format=format)
+            fi.histogram = build_histogram_file(fi.original, fi.histogram_path, HistogramType.FAST)
+        except Exception as e:
+            print("Creation of histogram representation could not be done")
+            print(e)
+            
 def test_mrxs_exists(image_path_mrxs):
-	# Test if the file exists, either locally either with the OAC
-	get_image(image_path_mrxs[0], image_path_mrxs[1])
-	assert os.path.exists(os.path.join(image_path_mrxs[0],image_path_mrxs[1])) == True
+    # Test if the file exists, either locally either with the OAC
+    path, filename = image_path_mrxs
+    get_image(path, filename)
+    assert os.path.exists(os.path.join(path,filename)) == True
 
 def test_mrxs_info(client, image_path_mrxs):
-	response = client.get(f'/image/upload_test_mrxs/{image_path_mrxs[1]}/info')
-	assert response.status_code == 200
-	assert "mrxs" in response.json()['image']['original_format'].lower()
-	assert response.json()['image']['width'] == 109240
-	assert response.json()['image']['height'] == 220696
+    _, filename = image_path_mrxs
+    response = client.get(f'/image/upload_test_mrxs/{filename}/info')
+    assert response.status_code == 200
+    assert "mrxs" in response.json()['image']['original_format'].lower()
+    assert response.json()['image']['width'] == 109240
+    assert response.json()['image']['height'] == 220696
 	
 def test_mrxs_metadata(client, image_path_mrxs):
-	response = client.get(f'/image/upload_test_mrxs/{image_path_mrxs[1]}/metadata')
-	assert response.status_code == 200
-	assert response.json()['items'][5]['key'] == 'ImageWidth'
-	assert response.json()['items'][5]["value"] == 854
-	assert response.json()['items'][6]['key'] == 'ImageHeight'
-	assert response.json()['items'][6]["value"] == 1724
-	assert response.json()['items'][2]["key"] == 'XResolution'
-	assert response.json()['items'][2]["value"] == 1
-	assert response.json()['items'][3]["key"] == 'YResolution'
-	assert response.json()['items'][3]["value"] == 1
-	assert response.json()['items'][1]["key"] == 'ResolutionUnit'
-	assert response.json()['items'][1]["value"] == "None"
+    _, filename = image_path_mrxs
+    response = client.get(f'/image/upload_test_mrxs/{filename}/metadata')
+    assert response.status_code == 200
+
+    lst = response.json()['items']
+
+    index = next((index for (index, d) in enumerate(lst) if d["key"] == "ImageWidth"), None)
+    assert response.json()['items'][index]["value"] == 854
+
+    index = next((index for (index, d) in enumerate(lst) if d["key"] == "ImageHeight"), None)
+    assert response.json()['items'][index]["value"] == 1724
+
+    index = next((index for (index, d) in enumerate(lst) if d["key"] == "XResolution"), None)
+    assert response.json()['items'][index]["value"] == 1
+
+    index = next((index for (index, d) in enumerate(lst) if d["key"] == "YResolution"), None)
+    assert response.json()['items'][index]["value"] == 1
+
+    index = next((index for (index, d) in enumerate(lst) if d["key"] == "ResolutionUnit"), None)
+    assert response.json()['items'][index]["value"] == "None"
 
 # For a non-normalized tile, the width is 124
 # To have a 256 x 256, the zoom level needs to be high enough
 def test_mrxs_norm_tile(client, image_path_mrxs):
-	response = client.get(f"/image/upload_test_mrxs/{image_path_mrxs[1]}/normalized-tile/zoom/4/ti/3", headers={"accept": "image/jpeg"})
-	assert response.status_code == 200
-	
-	img_response = Image.open(io.BytesIO(response.content))
-	width_resp, height_resp = img_response.size
-	assert width_resp == 256
-	assert height_resp == 256
+    _, filename = image_path_mrxs
+    response = client.get(f"/image/upload_test_mrxs/{filename}/normalized-tile/zoom/4/ti/3", headers={"accept": "image/jpeg"})
+    assert response.status_code == 200
+
+    img_response = Image.open(io.BytesIO(response.content))
+    width_resp, height_resp = img_response.size
+    assert width_resp == 256
+    assert height_resp == 256
 
 def test_mrxs_thumb(client, image_path_mrxs):
-	thumb_test(client, image_path_mrxs[1], "mrxs")
+    _, filename = image_path_mrxs
+    thumb_test(client, filename, "mrxs")
 		
 def test_mrxs_resized(client, image_path_mrxs):
-	resized_test(client, image_path_mrxs[1], "mrxs")	
+    _, filename = image_path_mrxs
+    resized_test(client, filename, "mrxs")	
 
 def test_mrxs_mask(client, image_path_mrxs):
-	mask_test(client, image_path_mrxs[1], "mrxs")
+    _, filename = image_path_mrxs
+    mask_test(client, filename, "mrxs")
 	
 def test_mrxs_crop(client, image_path_mrxs):
-	crop_test(client, image_path_mrxs[1], "mrxs")
-	
-@pytest.mark.skip
+    _, filename = image_path_mrxs
+    crop_test(client, filename, "mrxs")
+
+@pytest.mark.skip(reason='Does not return the correct response code')
 def test_mrxs_crop_null_annot(client, image_path_mrxs):
-	crop_null_annot_test(client, image_path_mrxs[1], "mrxs")
+    _, filename = image_path_mrxs
+    crop_null_annot_test(client, filename, "mrxs")
 
 def test_mrxs_histogram_perimage(client, image_path_mrxs):
-	histogram_perimage_test(client, image_path_mrxs[1], "mrxs")
+    _, filename = image_path_mrxs
+    histogram_perimage_test(client, filename, "mrxs")
--- tests/test_ndpi.py
+++ tests/test_ndpi.py
@@ -5,98 +5,137 @@ from fastapi import APIRouter
 from pims.formats import FORMATS
 import io
 from pims.importer.importer import FileImporter
-from tests.utils.formats import info_test, thumb_test, resized_test, mask_test, crop_test, crop_null_annot_test, histogram_perimage_test
-
+from utils.formats import info_test, thumb_test, resized_test, mask_test, crop_test, crop_null_annot_test, histogram_perimage_test
+from pims.formats.utils.factories import FormatFactory
+from pims.api.utils.models import HistogramType
+from pims.processing.histograms.utils import build_histogram_file
 from pims.files.file import (
     EXTRACTED_DIR, HISTOGRAM_STEM, ORIGINAL_STEM, PROCESSED_DIR, Path,
-    SPATIAL_STEM, UPLOAD_DIR_PREFIX, unique_name_generator
+    SPATIAL_STEM, UPLOAD_DIR_PREFIX
 )
 import pytest 
 
-def get_image(path, image):
-	filepath = os.path.join(path, image)
-	# If image does not exist locally -> download image
-	if not os.path.exists(path):
-		os.mkdir("/data/pims/upload_test_ndpi")
+def get_image(path, filename):
+    filepath = os.path.join(path,filename)
+    # If image does not exist locally -> download image
+    if not os.path.exists(path):
+        os.mkdir("/data/pims/upload_test_ndpi")
+	    
+    if not os.path.exists(filepath):
+        try:
+            url = f"https://data.cytomine.coop/open/uliege/{filename}"
+            urllib.request.urlretrieve(url, filepath)
+        except Exception as e:
+            print("Could not download image")
+            print(e)
 	
-	if not os.path.exists(filepath):
-		try:
-			url = f"https://data.cytomine.coop/open/uliege/{image}" #OAC
-			urllib.request.urlretrieve(url, filepath)
-		except Exception as e:
-			print("Could not download image")
-			print(e)
-	
-	if not os.path.exists(os.path.join(path, "processed")):
-		try:
-			fi = FileImporter(f"/data/pims/upload_test_ndpi/{image}")
-			fi.upload_dir = "/data/pims/upload_test_ndpi"
-			fi.processed_dir = fi.upload_dir / Path("processed")
-			fi.mkdir(fi.processed_dir)
-		except Exception as e:
-			print(path + "processed could not be created")
-			print(e)
-	if not os.path.exists(os.path.join(path, "processed/visualisation.NDPI")):
-		try:
-			fi.upload_path = Path(filepath)
-			original_filename = Path(f"{ORIGINAL_STEM}.NDPI")
-			fi.original_path = fi.processed_dir / original_filename
-			fi.mksymlink(fi.original_path, fi.upload_path)
-			spatial_filename = Path(f"{SPATIAL_STEM}.NDPI")
-			fi.spatial_path = fi.processed_dir / spatial_filename
-			fi.mksymlink(fi.spatial_path, fi.original_path)
-		except Exception as e:
-			print("Importation of images could not be done")
-			print(e)
+    if not os.path.exists(os.path.join(path, "processed")):
+        try:
+            fi = FileImporter(f"/data/pims/upload_test_ndpi/{filename}")
+            fi.upload_dir = "/data/pims/upload_test_ndpi"
+            fi.processed_dir = fi.upload_dir / Path("processed")
+            fi.mkdir(fi.processed_dir)
+        except Exception as e:
+            print(path + "processed could not be created")
+            print(e)
+    if not os.path.exists(os.path.join(path, "processed/visualisation.NDPI")):
+        if os.path.exists(os.path.join(path, "processed")):
+            fi = FileImporter(f"/data/pims/upload_test_ndpi/{filename}")
+            fi.upload_dir = "/data/pims/upload_test_ndpi"
+            fi.processed_dir = fi.upload_dir / Path("processed")
+        try:
+            fi.upload_path = Path(filepath)
+            original_filename = Path(f"{ORIGINAL_STEM}.NDPI")
+            fi.original_path = fi.processed_dir / original_filename
+            fi.mksymlink(fi.original_path, fi.upload_path)
+            spatial_filename = Path(f"{SPATIAL_STEM}.NDPI")
+            fi.spatial_path = fi.processed_dir / spatial_filename
+            fi.mksymlink(fi.spatial_path, fi.original_path)
+        except Exception as e:
+            print("Importation of images could not be done")
+            print(e)
+			
+    if not os.path.exists(os.path.join(path, "processed/histogram")):
+        if os.path.exists(os.path.join(path, "processed")):
+            fi = FileImporter(f"/data/pims/upload_test_ndpi/{filename}")
+            fi.upload_dir = Path("/data/pims/upload_test_ndpi")
+            fi.processed_dir = fi.upload_dir / Path("processed")
+            original_filename = Path(f"{ORIGINAL_STEM}.NDPI")
+            fi.original_path = fi.processed_dir / original_filename
+        try:
+            from pims.files.image import Image
+            fi.histogram_path = fi.processed_dir/Path(HISTOGRAM_STEM) #/data/pims/upload1641567540187798/processed/histogram
+            format = FormatFactory().match(fi.original_path)
+            fi.original = Image(fi.original_path, format=format)
+            fi.histogram = build_histogram_file(fi.original, fi.histogram_path, HistogramType.FAST)
+        except Exception as e:
+            print("Creation of histogram representation could not be done")
+            print(e)
 			
 def test_ndpi_exists(image_path_ndpi):
 	# Test if the file exists, either locally either with the OAC
-	get_image(image_path_ndpi[0], image_path_ndpi[1])
-	assert os.path.exists(os.path.join(image_path_ndpi[0],image_path_ndpi[1])) == True
+	path, filename = image_path_ndpi
+	get_image(path, filename)
+	assert os.path.exists(os.path.join(path, filename)) == True
 
 def test_ndpi_info(client, image_path_ndpi):
-	response = client.get(f'/image/upload_test_ndpi/{image_path_ndpi[1]}/info')
-	assert response.status_code == 200
-	assert "ndpi" in response.json()['image']['original_format'].lower()
-	assert response.json()['image']['width'] == 71424
-	assert response.json()['image']['height'] == 24064
-	
+    path, filename = image_path_ndpi 
+    response = client.get(f'/image/upload_test_ndpi/{filename}/info')
+    assert response.status_code == 200
+    assert "ndpi" in response.json()['image']['original_format'].lower()
+    assert response.json()['image']['width'] == 71424
+    assert response.json()['image']['height'] == 24064
+
 def test_metadata(client, image_path_ndpi):
-	response = client.get(f'/image/upload_test_ndpi/{image_path_ndpi[1]}/metadata')
-	assert response.status_code == 200
-	assert response.json()['items'][8]["key"] == 'XResolution'
-	assert response.json()['items'][8]["value"] == '(43784, 1)'
-	assert response.json()['items'][9]["key"] == 'YResolution'
-	assert response.json()['items'][9]["value"] == '(43784, 1)'
-	assert response.json()['items'][10]["key"] == 'ResolutionUnit'
-	assert response.json()['items'][10]["value"] == "CENTIMETER"
+    _, filename = image_path_ndpi 
+    response = client.get(f'/image/upload_test_ndpi/{filename}/metadata')
+    assert response.status_code == 200
+
+    assert response.status_code == 200
+    lst = response.json()['items']
+
+    index = next((index for (index, d) in enumerate(lst) if d["key"] == "XResolution"), None)
+    assert response.json()['items'][index]["value"] == '(43784, 1)'
+
+    index = next((index for (index, d) in enumerate(lst) if d["key"] == "YResolution"), None)
+    assert response.json()['items'][index]["value"] == '(43784, 1)'
+
+    index = next((index for (index, d) in enumerate(lst) if d["key"] == "ResolutionUnit"), None)
+    assert response.json()['items'][index]["value"] == "CENTIMETER"
 
 # For a non-normalized tile, the width is 124
 def test_ndpi_norm_tile(client, image_path_ndpi):
-	response = client.get(f"/image/upload_test_ndpi/{image_path_ndpi[1]}/normalized-tile/zoom/3/ti/3", headers={"accept": "image/jpeg"})
-	assert response.status_code == 200
-	
-	img_response = Image.open(io.BytesIO(response.content))
-	width_resp, height_resp = img_response.size
-	assert width_resp == 256
-	assert height_resp == 256
-	
+    _, filename = image_path_ndpi 
+    response = client.get(f"/image/upload_test_ndpi/{filename}/normalized-tile/zoom/3/ti/3", headers={"accept": "image/jpeg"})
+    assert response.status_code == 200
+
+    img_response = Image.open(io.BytesIO(response.content))
+    width_resp, height_resp = img_response.size
+    assert width_resp == 256
+    assert height_resp == 256
+
 def test_ndpi_thumb(client, image_path_ndpi):
-	thumb_test(client, image_path_ndpi[1], "ndpi")
-		
+    _, filename = image_path_ndpi 
+    thumb_test(client, filename, "ndpi")
+	
 def test_ndpi_resized(client, image_path_ndpi):
-	resized_test(client, image_path_ndpi[1], "ndpi")
+    _, filename = image_path_ndpi 
+    resized_test(client, filename, "ndpi")
 	
 def test_ndpi_mask(client, image_path_ndpi):
-	mask_test(client, image_path_ndpi[1], "ndpi")
+    _, filename = image_path_ndpi 
+    mask_test(client, filename, "ndpi")
 		
 def test_ndpi_crop(client, image_path_ndpi):
-	crop_test(client, image_path_ndpi[1], "ndpi")
+    _, filename = image_path_ndpi 
+    crop_test(client, filename, "ndpi")
 
-@pytest.mark.skip
+@pytest.mark.skip(reason="Does not return the correct response code")
 def test_ndpi_crop_null_annot(client, image_path_ndpi):
-	crop_null_annot_test(client, image_path_ndpi[1], "ndpi")
-	
+    _, filename = image_path_ndpi 
+    crop_null_annot_test(client, filename, "ndpi")
+
 def test_ndpi_histogram_perimage(client, image_path_ndpi):
-	histogram_perimage_test(client, image_path_ndpi[1], "ndpi")
+    _, filename = image_path_ndpi 
+    histogram_perimage_test(client, filename, "ndpi")
 	
