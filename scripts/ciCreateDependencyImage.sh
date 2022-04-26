24e64d95b1346f256f6bffeb1d307bacc0ca15e3
chlmarchal <chloe.marchal@cytomine.com>
2022-04-26 17:25:26 +0200

    Revert "clone pims in debug mode"
    This reverts commit 9a68c2002b91700bbc8ad933295996c60ac2e10d.

--- scripts/ciCreateDependencyImage.sh
+++ scripts/ciCreateDependencyImage.sh
@@ -10,7 +10,7 @@ VERSION_NUMBER=$(<"$file")
 
 echo "Launch Create dependency image for $VERSION_NUMBER"
 
-git clone --branch debug_tests_plugins --depth 1 https://github.com/cytomine/pims ./ci/app
+git clone --branch master --depth 1 https://github.com/cytomine/pims ./ci/app
 
 mkdir -p ./ci/app/plugins/pims-plugin-format-openslide/
 cp -r ./pims_plugin_format_openslide ./ci/app/plugins/pims-plugin-format-openslide/
